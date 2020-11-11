e54cede1c059f36afa9fe1aad75c0624906c2409
Papi Diagne <abdiagne96@gmail.com>
2020-11-11 16:27:59 +0000

    added overview and search feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import Movie from './components/Movie'
+import Movie from "./components/Movie";
 
 function App() {
-  const moviesJSON = require('./movies-in-theaters.json')
-  const [movies, setMovies] = useState([])
+  const moviesJSON = require("./movies-in-theaters.json");
+  const [movies, setMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
-    setMovies(moviesJSON)
-  }, [moviesJSON])
+    setFilteredMovies(
+      movies.filter((movie) => {
+        return movie.title.toLowerCase().includes(searchQuery.toLowerCase());
+      })
+    );
+    console.log(filteredMovies);
+  }, [searchQuery]);
+
+  useEffect(() => {
+    setMovies(moviesJSON);
+  }, [moviesJSON]);
 
   return (
-    <div className="movieContainer">
-      {movies.length > 0 && movies.map(movie => (<Movie key={movie.id} {...movie}/>))}
+    <div className="rootContainer">
+      <header>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </header>
+      <div className="movieContainer">
+        {movies.length > 0 &&
+          (searchQuery == ""
+            ? movies.map((movie) => <Movie key={movie.id} {...movie} />)
+            : filteredMovies.map((movie) => (
+                <Movie key={movie.id} {...movie} />
+              )))}
+      </div>
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Movie = ({ title, posterurl, year, storyline, ratings }) => {
-    const averageRating = (ratings.reduce((a, b) => (a + b)) / ratings.length).toFixed(1)
-    return (
-        <div className="movie">
-            <img className="movieImg" src={posterurl} alt={title} />
-            <div className="movieInfo">
-                <h3>{title}</h3>
-                <span>{ averageRating }</span>
-            </div>
-        </div>
-    )
-}
+  const averageRating = (
+    ratings.reduce((a, b) => a + b) / ratings.length
+  ).toFixed(1);
+  return (
+    <div className="movie">
+      <img className="movieImg" src={posterurl} alt={title} />
+      <div className="movieInfo">
+        <h3>{title}</h3>
+        <span>{averageRating}</span>
+      </div>
+      <div className="movieOver">
+        <h2>Overview:</h2>
+        <p>{storyline}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Movie
+export default Movie;
